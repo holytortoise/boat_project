5d832303b65c14dac36d3897b8de029720b86e84
holytortoise <nick.white@outlook.de>
2018-06-17 20:58:15 +0200

    c

--- src/boat/boat/settings.py
+++ src/boat/boat/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
+    'schedule',
     'account',
     'reservierung',
 ]
@@ -126,6 +127,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,"static_root/")
--- src/boat/reservierung/views.py
+++ src/boat/reservierung/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from calendar import HTMLCalendar
+
 
 # Create your views here.
 def index(request):
-    calendar = HTMLCalendar().formatyear(2018)
-    context_dict={'calendar':calendar}
-    return render(request, 'index.html',context_dict)
+    return render(request, 'index.html')
