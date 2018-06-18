32c9dd29d837eb28eb50edb978b48068e4141962
holytortoise <holytortoise@users.noreply.github.com>
2018-06-18 14:41:12 +0200

    c

--- src/boat/reservierung/forms.py
+++ src/boat/reservierung/forms.py
@@ -9,3 +9,25 @@ class ReservierungForm(forms.Form):
     e_Datum = forms.DateField()
     a_Zeit = forms.TimeField(help_text='HH:mm')
     e_Zeit = forms.TimeField(help_text='HH:mm')
+
+    def clean(self):
+        cleaned_data = super(ReservierungForm, self).clean()
+        a_Datum = cleaned_data.get('a_Datum')
+        e_Datum = cleaned_data.get('e_Datum')
+        a_Zeit = cleaned_data.get('a_Zeit')
+        e_Zeit = cleaned_data.get('e_Zeit')
+
+        if a_Datum and e_Datum:
+            # Only do something if both fields are valid so far
+            if a_Datum < datetime.date.today():
+                raise forms.ValidationError("Anfangsdatum kann nicht in der Vergangenheit liegen.")
+            if a_Datum == datetime.date.today():
+                if a_Zeit < datetime.datetime.time():
+                    raise forms.ValidationError("Anfangszeit kann nicht in der Vergangenheit liegen.")
+            if a_Datum > e_Datum:
+                raise forms.ValidationError("Enddatum kann nicht vor Anfangsdatum sein.")
+            if a_Datum == e_Datum:
+                if a_Zeit > e_Datum:
+                    raise forms.ValidationError("Anfangszeit kann nicht nach der Endzeit liegen.")
+                if a_Zeit == e_Zeit:
+                    raise forms.ValidationError("Anfangs und End Zeit können nicht gleich sein.")
--- src/boat/reservierung/views.py
+++ src/boat/reservierung/views.py
@@ -12,4 +12,12 @@ def index(request):
 class ReservierungsList(ListView):
     queryset = models.Reservierung.objects.order_by('a_Datum','a_Zeit')
     context_object_name = 'reservierungen'
+
+
+
+def reservierung_form(request):
+    """
+    Diese Funktion ist für die Reservierung zuständig
+    """
+    nutzer = request.user
     
