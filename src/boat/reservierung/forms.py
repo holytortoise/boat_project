3b3ee371e8a22049bb40c8576277c04ecb5196f0
holytortoise <nick.white@outlook.de>
2018-06-30 17:34:29 +0200

    c

--- src/boat/reservierung/forms.py
+++ src/boat/reservierung/forms.py
@@ -6,7 +6,7 @@ from .models import Reservierung,Boot
 class ReservierungForm(forms.Form):
     form_choice = ()
     form_choice = create_choice()
-    reserviertesBoot = forms.ChoiceField(label='Boot',form_choice)
+    reserviertesBoot = forms.ChoiceField(form_choice,label='Boot')
     a_Datum = forms.DateField(label='Anfangs Datum',initial='Anfangs Datum')
     e_Datum = forms.DateField(label='End Datum')
     a_Zeit = forms.TimeField(label='Anfangs Zeit',help_text='HH:mm')
