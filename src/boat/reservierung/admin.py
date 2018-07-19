a8b7fcd90901638ca96f580ccd7fde7073db9f92
holytortoise <holytortoise@users.noreply.github.com>
2018-07-19 10:22:59 +0200

    c

--- src/boat/reservierung/admin.py
+++ src/boat/reservierung/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Boot,Reservierung,Einweisung,Images
+from .models import Boot,Reservierung,Einweisung,Images,Instandsetzung
 # Register your models here.
 admin.site.register(Boot)
 admin.site.register(Reservierung)
 admin.site.register(Einweisung)
 admin.site.register(Images)
+admin.site.register(Instandsetzung)
