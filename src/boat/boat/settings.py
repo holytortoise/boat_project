74c49fca5a233e2d5b4d0222c6912bbaa0b73a27
holytortoise <nick.white@outlook.de>
2018-07-21 20:02:26 +0200

    c

--- src/boat/boat/settings.py
+++ src/boat/boat/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -133,6 +134,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,"static_root/")
 STATICFILES_DIRS = [STATIC_DIR,]
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = 'reservierung:index'
 LOGOUT_REDIRECT_URL = 'reservierung:index'
