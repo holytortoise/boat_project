a3046d252fc10e5e4d3bc0b2e6f141905e69cae3
holytortoise <nick.white@outlook.de>
2019-04-03 17:58:54 +0200

    add automatic_delete

---
+++ src/boat/reservierung/management/commands/alter_reservierungen.py
@@ -0,0 +1,28 @@
+import os
+import datetime
+import dateutil.relativedelta as relativedelta
+
+from django.core.management.base import BaseCommand, CommandError
+from django.db import models as m
+
+from reservierung import models
+
+
+class Command(BaseCommand):
+    help = "Entfernen alter Reservierungen"
+
+    def add_arguments(self,parser):
+        pass
+
+    def handle(self, *args, **options):
+        alter_reservierungen()
+
+
+def alter_reservierungen():
+    """
+    Entfernt alte Reservierungen
+    """
+    reservierungen = models.Reservierung.objects.all()
+    for reservierung in reservierungen:
+        if relativedelta.relativedelta(datetime.datetime.now(),reservierung.e_Datum).months > 2:
+            reservierung.delete()
